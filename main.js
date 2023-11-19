b60ea97191e3020d54d5407bbb3c01fc54cc68c9
Beshoy Samy <120126754+SamyBeshoy@users.noreply.github.com>
2023-11-19 19:02:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+document.title = "project-js-9";
+document.body.style.cssText = `
+height: 100vh;
+margin: 0;
+padding: 0;
+font-family: sans-serif;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`;
+let Likes = document.getElementById("Likes");
+let add = document.getElementById("+");
+let remove = document.getElementById("-");
+let num = window.localStorage.getItem("number");
+window.onload = function (params) {
+        if (window.localStorage.getItem("number") !== null) {
+                Likes.innerHTML = `${window.localStorage.getItem("number")}`;
+                console.log("localStorage Not Empty")
+        }
+}
+add.onclick = function (params) {
+        window.localStorage.setItem("number", ++num)
+        if (window.localStorage.getItem("number") !== null) {
+                Likes.innerHTML = window.localStorage.getItem("number");
+        }
+}
+remove.onclick = function (params) {
+        window.localStorage.setItem("number", --num)
+        if (window.localStorage.getItem("number") !== null) {
+                Likes.innerHTML = window.localStorage.getItem("number");
+        }
+}
